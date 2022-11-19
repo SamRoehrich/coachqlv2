3f119fd88b88c80f5ffe471bbe58ca4efd1e809a
SamRoehrich <sam_roehrich@icloud.com>
2022-11-19 15:16:22 -0700

    testing links

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
 	return (
 		<div className="p-8 bg-gray-100">
-			<h1 className="text-red-500">My Blog</h1>
+			<h1 className="text-red-500">:ahhhh:</h1>
 		</div>
 	)
 }
